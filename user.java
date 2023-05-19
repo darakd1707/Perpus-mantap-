c8dba217a5a4dba8780de375e628043e9188011b
darakd1707 <110018761+darakd1707@users.noreply.github.com>
2023-05-19 13:27:56 +0700

    user.java

---
+++ user.java
@@ -0,0 +1,15 @@
+
+public interface user {
+
+    public void setnama(String nama);
+
+    public void setalamat(String alamat);
+
+    public void settelepon(String telepon);
+
+    public String getnama(int id);
+
+    public String getalamat(int id);
+
+    public String gettelepon(int id);
+}
